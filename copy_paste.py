180c797e35d1d267e2fd8333974ba94c72f31dd8
Glass <125502088+Glasssparrow@users.noreply.github.com>
2024-09-25 14:28:33 +0300

    copy folder

--- copy_paste.py
+++ copy_paste.py
@@ -1,5 +1,9 @@
 import os
+import shutil
 
 source = "test_file.txt"
 destination = "test_copy.txt"
-os.system(f'copy {source} {destination}')
+source_folder = "test_folder1"
+destination_folder = "test_folder2"
+# shutil.copy(source, destination1) 
+shutil.copytree(source, destination1)
