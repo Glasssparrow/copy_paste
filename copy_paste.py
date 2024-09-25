cfb803c73bc7da2f311daabcf10a28eed89602ee
Glass <125502088+Glasssparrow@users.noreply.github.com>
2024-09-25 14:33:11 +0300

    remove directory

--- copy_paste.py
+++ copy_paste.py
@@ -6,4 +6,5 @@ destination = "test_copy.txt"
 source_folder = "test_folder1"
 destination_folder = "test_folder2"
 # shutil.copy(source, destination1) 
-shutil.copytree(source, destination1)
+# shutil.copytree(source, destination1)
+shutil.rmtree(source_folder)
