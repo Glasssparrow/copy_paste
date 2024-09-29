65930596a9614de2bdc76f66406860ede7041908
Glass <125502088+Glasssparrow@users.noreply.github.com>
2024-09-29 15:08:12 +0300

    nothing

--- Code/order.py
+++ Code/order.py
@@ -2,7 +2,9 @@ from Code.get_list_of_folders_or_files import (
     get_list_of_folders_names,
     get_list_of_files_names,
 )
-from Code.get_list_of_string_from_file import get_list_of_strings_from_file
+from Code.get_list_of_string_from_file import (
+    get_list_of_strings_from_file,
+)
 import os
 
 
