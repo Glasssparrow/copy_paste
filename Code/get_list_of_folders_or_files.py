fc5e2831bfa076bab74262524bb2fc966072ccbb
Glass <125502088+Glasssparrow@users.noreply.github.com>
2024-09-29 14:19:04 +0300

    get files names

--- Code/get_list_of_folders_or_files.py
+++ Code/get_list_of_folders_or_files.py
@@ -17,3 +17,21 @@ def get_list_of_folders_names(directory_name):
 
     result.sort()
     return result
+
+
+def get_list_of_files_names(directory_name):
+    # get all files' and folders' names in the current directory
+    filenames = os.listdir(directory_name)
+
+    result = []
+    for filename in filenames:  # loop through all the files and folders
+        path = os.path.join(
+            os.path.abspath(directory_name), filename
+        )
+        if not os.path.isdir(os.path.join(
+                directory_name, filename
+        )):  # check whether the current object is a folder or not
+            result.append(filename)
+
+    result.sort()
+    return result
