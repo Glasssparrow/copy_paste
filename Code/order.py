14c5c7c6284acf03e016283e4ce5fa1ec24e3335
Glass <125502088+Glasssparrow@users.noreply.github.com>
2024-09-29 15:06:17 +0300

    Update order.py

--- Code/order.py
+++ Code/order.py
@@ -3,6 +3,7 @@ from Code.get_list_of_folders_or_files import (
     get_list_of_files_names,
 )
 from Code.get_list_of_string_from_file import get_list_of_strings_from_file
+import os
 
 
 class Order:
@@ -14,4 +15,5 @@ class Order:
 def get_orders():
     folders_with_orders = get_list_of_folders_names("Data")
     for folder in folders_with_orders:
-        pass
+        path = os.path.join("Data", folder)
+        files = get_list_of_files_names(path)
