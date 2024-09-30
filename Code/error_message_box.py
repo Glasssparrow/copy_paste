e6bfaf0a2ca45d5aa0cf9fb822b9a6fa47b08dcf
Glass <125502088+Glasssparrow@users.noreply.github.com>
2024-09-30 15:49:41 +0300

    Update error_message_box.py

---
+++ Code/error_message_box.py
@@ -0,0 +1,23 @@
+from tkinter import Button, Tk, Label, Checkbutton, BooleanVar
+from datetime import datetime
+
+
+class ErrorGui:
+
+    title_text = "Error message box"
+
+    def __init__(self, message_tuple):
+        # Оформление окна
+        self._window = Tk()
+        self._window.title(self.title_text)
+        self._window.geometry("500x100")
+        self.message_text = ""
+        for tuple_element in message_tuple:
+            self.message_text += tuple_element
+
+
+        # Сообщение об ошибке
+        self._text_path = Label(text=self.message_text)
+        self._text_path.grid(column=0, row=0)
+        # Запускаем окно
+        self._window.mainloop()
