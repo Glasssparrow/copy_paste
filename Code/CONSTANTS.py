2d7057878509f691769fa753f90ee19dcd4fa1da
Glass <125502088+Glasssparrow@users.noreply.github.com>
2024-09-30 15:57:52 +0300

    Update CONSTANTS.py

--- Code/CONSTANTS.py
+++ Code/CONSTANTS.py
@@ -3,3 +3,4 @@
 TARGET_DIRECTORIES_STORAGE = "target_directories.txt"
 FILE_EXTENSIONS_STORAGE = "file_extensions.txt"
 LAST_PARTS_OF_NAMES_STORAGE = "last_parts_of_the_names.txt"
+TARGET_FOLDER_STORAGE = "target_folders_inside_target_directory.txt"
