a1b33185cdc40d9487e9931f3b157d839ddf4381
Glass <125502088+Glasssparrow@users.noreply.github.com>
2024-09-30 16:26:24 +0300

    Update test.py

--- test.py
+++ test.py
@@ -2,6 +2,13 @@ from Code.get_list_of_folders_or_files import (
     get_list_of_folders_names
 )
 from Code.order import get_orders
+from Code.error_message_box import ErrorGui
+from Code.gui import Gui
 
 
-orders = get_orders("Data")
+try:
+    orders = get_orders("Data")
+except Exception as error:
+    gui = ErrorGui(error.args)
+else:
+    gui = Gui()
