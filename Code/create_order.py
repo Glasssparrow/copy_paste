a8a9b81a961507249859a2b7f0e564e26df501ff
Glass <125502088+Glasssparrow@users.noreply.github.com>
2024-11-10 18:35:37 +0300

    Update create_order.py

--- Code/create_order.py
+++ Code/create_order.py
@@ -1,5 +1,13 @@
 from Code.order import *
 
 
+def add_element_to_order(order, element, is_folder: bool):
+    pass
+
+
 def get_order(rules, folders, files):
     order = Order()
+    for folder in folders:
+        add_element_to_order(order, folder, True)
+    for file in files:
+        add_element_to_order(order, file, False)
