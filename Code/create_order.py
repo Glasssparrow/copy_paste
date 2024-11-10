a8b2ed28ba3bbc45a2cef1ec0e3e21a0ae7335c8
Glass <125502088+Glasssparrow@users.noreply.github.com>
2024-11-10 18:43:03 +0300

    Update create_order.py

--- Code/create_order.py
+++ Code/create_order.py
@@ -1,13 +1,21 @@
 from Code.order import *
 
 
-def add_element_to_order(order, element, is_folder: bool):
-    pass
+def add_element_to_order(rules, order, element, is_folder: bool):
+    for rule in rules:
+        if rule.should_be_copied(element, is_folder):
+            pass
 
 
 def get_order(rules, folders, files):
     order = Order()
     for folder in folders:
-        add_element_to_order(order, folder, True)
+        add_element_to_order(
+            rules=rules, order=order,
+            element=folder, is_folder=True,
+        )
     for file in files:
-        add_element_to_order(order, file, False)
+        add_element_to_order(
+            rules=rules, order=order,
+            element=file, is_folder=False,
+        )
