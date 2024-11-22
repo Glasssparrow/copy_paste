d64e94f21ce8fb7575c8c0c4a3a2407af7c1214b
Glass <125502088+Glasssparrow@users.noreply.github.com>
2024-11-22 20:50:05 +0300

    fix creating folders

--- Code/execute_order.py
+++ Code/execute_order.py
@@ -10,17 +10,22 @@ def copy_files(order, project_folder, source_folder):
         for paths_for_rule in rules_for_file.data:
             for folders_for_path in paths_for_rule.data:
                 for folder in folders_for_path.data:
-                    target = path.join(
+                    target_path = path.join(
                         folders_for_path.path,
                         project_folder,
                         folder.folder,
-                        rules_for_file.name,
-                    )
-                    source = path.join(
-                        source_folder,
-                        rules_for_file.name,
                     )
                     if rules_for_file.is_folder:
-                        copy_folder(source, target)
+                        copy_folder(
+                            source_path=source_folder,
+                            source_name=rules_for_file.name,
+                            target_path=target_path,
+                            target_name=rules_for_file.name,
+                        )
                     else:
-                        copy_file(source, target)
+                        copy_file(
+                            source_path=source_folder,
+                            source_name=rules_for_file.name,
+                            target_path=target_path,
+                            target_name=rules_for_file.name,
+                        )
